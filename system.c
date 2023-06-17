e29c2afa487dbdc0f4849c388e2c9fce9ce3a05b
tungYang7 <yangming3.14159@gmail.com>
2023-06-17 16:55:28 +0800

    feat: update on system and fix bug on func.h

--- system.c
+++ system.c
@@ -45,7 +45,7 @@ int special_show(System *system, int32_t i){
 
 void print_userMsg(Player player_list[], System *system){
         printf(BLUE"自己: \n");
-        printf("\t你的資源: %d\n", player_list[0].total_resource);
+        printf("\t你的資源: %d\n"NONE, player_list[0].total_resource);
         if( special_show(system, 0) ){
             printf("\t你的成就: ");
             if(system->lr_player == 0){
@@ -58,10 +58,31 @@ void print_userMsg(Player player_list[], System *system){
         }
 }
 
+void print_allResource(Player player_list[], System *system){
+    const char resource_name[][7] = {"小麥", "木頭", "羊毛", "石頭", "磚頭"};
+    printf(BLUE"你擁有的資源: %d\n", player_list[0].total_resource);
+    for(int32_t i = 0; i < 5; ++i){
+        printf("  %s: %d\t", resource_name[i], player_list[0].resource[i]);
+    }
+    printf(NONE);
+    printf("其他玩家擁有的資源: \n");
+    for(int32_t i = 1; i < system->player_num; ++i){
+        if(i == 1)          printf(RED);
+        else if(i == 2)     printf(GREEN);
+        else if(i == 3)     printf(YELLOW);
+        printf("  玩家%d: %d\t"NONE, i, player_list[i].total_resource);
+    }
+    printf("\n");
+}
+
+
 void print_playerMsg(Player player_list[], System *system){
     for(int32_t i = 1; i<system->player_num; ++i){
-        printf(BLUE"玩家%d:\n", i);
-        printf("\t玩家資源: %d\n", player_list[i].total_resource);
+        if(i == 1)          printf(RED);
+        else if(i == 2)     printf(GREEN);
+        else if(i == 3)     printf(YELLOW);
+        printf("玩家%d:\n", i);
+        printf("\t玩家資源: %d\n"NONE, player_list[i].total_resource);
         if( special_show(system, i) ){
             printf("\t成就: ");
             if(system->lr_player == i){
@@ -73,7 +94,7 @@ void print_playerMsg(Player player_list[], System *system){
             printf("\n");
         }
     }
-    print_playerMsg(player_list, system);
+    print_userMsg(player_list, system);
     return ;
 }
 
